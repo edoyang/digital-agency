025d832b9ef33df1e1447de820249c131bec5f45
unknown <edoyangz@gmail.com>
2023-11-11 23:16:50 +1100

    card finishing

--- script.js
+++ script.js
@@ -49,33 +49,46 @@ services.forEach(service => {
 });
 
 const card = [
-    { label: "Lorem Ipsum", title :"Lorem Ipsum", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", button: true },
-    { label: "Lorem Ipsum", title :"Lorem Ipsum", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", button: true },
-    { label: "Lorem Ipsum", title :"Lorem Ipsum", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", button: true },
-    { label: "Lorem Ipsum", title :"Lorem Ipsum", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", button: true },
-    { label: "Lorem Ipsum", title :"Lorem Ipsum", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", button: true },
+    { href:"#", label: "HTML & CSS",label_color:"#0CBE64", title :"Lorem Ipsum", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", button: true },
+    { href:"#", label: "Javascript",label_color:"#DC0000", title :"Lorem Ipsum", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", button: true },
+    { href:"#", label: "API",label_color:"#B000DC", title :"Lorem Ipsum", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", button: true },
+    { href:"#", label: "React.js",label_color:"#0058DC", title :"Lorem Ipsum", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", button: true },
+    { href:"#", label: "Angular.js",label_color:"#DC7000", title :"Lorem Ipsum", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", button: true },
+    { href:"#", label: "Vue.js",label_color:"#DC7000", title :"Lorem Ipsum", body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", button: true }
 ];
+
+function hexToRGBA(hex, alpha) {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
 
 function createCardItem(card) {
     const itemDiv = document.createElement('div');
     itemDiv.className = 'card';
 
     const label = document.createElement('p');
+    label.className = 'label';
     label.textContent = card.label;
-    
+    label.style.color = card.label_color;
+    label.style.backgroundColor = hexToRGBA(card.label_color, 0.1);
     itemDiv.appendChild(label);
 
     const title = document.createElement('h1');
     title.textContent = card.title;
-    itemDiv.appendChild(title);  // Fixed this line
+    title.className = 'title';
+    itemDiv.appendChild(title);
 
     const body = document.createElement('p');
     body.textContent = card.body;
-    itemDiv.appendChild(body);  // Fixed this line
+    body.className = 'body';
+    itemDiv.appendChild(body);
 
     if (card.button) {
         const anchor = document.createElement('a');
-        anchor.href = '#';
+        anchor.href = card.href;
         anchor.className = 'read-more-button';
         anchor.style.textDecoration = 'none';
     
@@ -105,4 +118,49 @@ function createCardItem(card) {
 const cardContainer = document.querySelector('.product__cards');
 card.forEach(card => {
     cardContainer.appendChild(createCardItem(card));
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const productCardsContainer = document.querySelector('.product__cards');
+    let cards = document.querySelectorAll('.product__cards .card');
+
+    // Function to clone all cards in the set
+    const cloneCards = () => {
+        for (let i = 0; i < cards.length; i++) {
+            const clone = cards[i].cloneNode(true);
+            productCardsContainer.appendChild(clone);
+        }
+        // Update the cards NodeList
+        cards = document.querySelectorAll('.product__cards .card');
+    };
+
+    // Scroll Right by 300px
+    const scrollRight = () => {
+        productCardsContainer.scrollLeft += 300;
+        checkAndCloneCards();
+    };
+
+    // Scroll Left by 300px
+    const scrollLeft = () => {
+        productCardsContainer.scrollLeft -= 300;
+        checkAndCloneCards();
+    };
+
+    // Check if need to clone cards (based on scroll position)
+    const checkAndCloneCards = () => {
+        const maxScrollLeft = productCardsContainer.scrollWidth - productCardsContainer.clientWidth;
+        if (productCardsContainer.scrollLeft > maxScrollLeft - 300) {
+            cloneCards();
+        }
+    };
+
+    // Attach event listeners to arrow buttons
+    const arrowLeft = document.querySelector('.arrow__button img[alt="arrow_left"]');
+    const arrowRight = document.querySelector('.arrow__button img[alt="arrow_right"]');
+
+    arrowLeft.addEventListener('click', scrollLeft);
+    arrowRight.addEventListener('click', scrollRight);
+
+    // Initial cloning of cards
+    cloneCards();
 });
